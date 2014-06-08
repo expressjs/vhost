ba76903fd078bc50e5ed09a88084d55894e6c6be
Douglas Christopher Wilson <doug@somethingdoug.com>
2014-06-08 13:51:23 -0400

    Remove old invocation of server.onvhost
    This was there for express < 3 and doesn't even make sense based
    on the current middleware pattern.

--- HISTORY.md
+++ HISTORY.md
@@ -1,3 +1,8 @@
+2.x
+===
+
+  * Remove old invocation of `server.onvhost`
+
 1.0.0 / 2014-03-05
 ==================
 
--- index.js
+++ index.js
@@ -29,7 +29,6 @@ module.exports = function vhost(hostname, server){
   if (!hostname) throw new Error('vhost hostname required');
   if (!server) throw new Error('vhost server required');
   var regexp = new RegExp('^' + hostname.replace(/[^*\w]/g, '\\$&').replace(/[*]/g, '(?:.*?)')  + '$', 'i');
-  if (server.onvhost) server.onvhost(hostname);
   return function vhost(req, res, next){
     if (!req.headers.host) return next();
     var host = req.headers.host.split(':')[0];
